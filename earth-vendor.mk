6a702049ed0b6a8304f0766d894de4a592d365fb
Giovanni Ricca <giovanniricca@proton.me>
2025-10-06 12:21:13 +0200

    earth: Enable ELF checks for libimsma

--- earth-vendor.mk
+++ earth-vendor.mk
@@ -835,7 +835,7 @@ PRODUCT_PACKAGES += \
     libmtk_vt_service \
     libmtk_vt_wrapper \
     libsignal \
-    libsink \
+    libsink-mtk \
     libsource \
     libvcodec_cap \
     libvcodec_capenc \
